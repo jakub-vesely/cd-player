c27b0cb0473522f0b11611098ee49ad73e158455
Jakub Vesely <jakub.vesely@electrolux.com>
2019-06-25 09:05:13 +0200

    "implemented" de_info interface for win

--- src/system/cd_info_base.py
+++ src/system/cd_info_base.py
@@ -1,3 +1,3 @@
 class CdInfoBase():
-    def get_track_count(src):
-        NotImplemented
+    def __init__(self, _stop_event, cd_track_count_callback, _cd_track_names_callback):
+        NotImplementedError
