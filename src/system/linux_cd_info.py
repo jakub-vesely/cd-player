2186afbbb0b7adfd9cee979d5d83023e45353abd
Jakub Vesely <jakub_vesely@seznam.cz>
2019-11-25 06:50:09 +0000

    added cd-text option how to load cd info

--- src/system/linux_cd_info.py
+++ src/system/linux_cd_info.py
@@ -1,23 +1,59 @@
 import logging
 from src.system.cd_info_base import CdInfoBase
-from subprocess import Popen, PIPE, STDOUT, DEVNULL
+from subprocess import Popen, PIPE, STDOUT, DEVNULL, TimeoutExpired
 from threading import Thread, Event, Lock
 from time import sleep
 
 class LinuxCdInfo(CdInfoBase):
     def __init__(self):
         super().__init__()
+        self.last_disc_id = None
+        self.last_track_names = None
 
     def get_current_folder_content(self):
         disc_id = self._get_disc_id()
+        if disc_id == self.last_disc_id:
+            return self.last_track_names
+        self.last_disc_id = disc_id
+
+        self.last_track_names = list()
         track_count = self._get_track_count(disc_id)
         if track_count:
-            track_names = self._get_track_names(disc_id)
-            if not track_names:
-                for i in range(track_count):
-                    track_names.append(f"Track{i+1}")
-            return track_names
-        return list()
+            track_names = self._get_track_names_from_cddb(disc_id)
+            self.last_track_names = track_names
+
+        if not self.last_track_names: #second option it can take a time if there is not cd_text
+            cd_text_names = self._get_track_names_from_cd_text()
+            if cd_text_names:
+                self.last_track_names = cd_text_names
+
+        if not self.last_track_names: #last option
+            for i in range(track_count):
+                self.last_track_names.append(f"Track{i+1}")
+
+        return self.last_track_names 
+
+    def _get_track_names_from_cd_text(self):
+        subprocess = Popen(["cd-info"], stdout=PIPE, stderr=DEVNULL) #I do not want to display errors when cdrom is not connected
+        try:
+            stdout = str( subprocess.communicate(timeout=10)[0], "ascii") #10 sec should be enough for the case when data is available
+        except TimeoutExpired:
+            subprocess.kill()
+            return list()
+
+        track_info_pos =  stdout.find("CD-TEXT for Track")
+        if track_info_pos == -1:
+            return list()
+
+        track_names = list()
+        track_info_lines = stdout[track_info_pos:].split("\n")
+        for line in track_info_lines:
+            title_prefix = "TITLE: "
+            title_prefix_pos = line.find(title_prefix)
+            if title_prefix_pos == -1:
+                continue
+            track_names.append(line[title_prefix_pos + len(title_prefix):])
+        return track_names
 
     def _get_disc_id(self):
         subprocess = Popen(["cd-discid"], stdout=PIPE, stderr=DEVNULL) #I do not want to display errors when cdrom is not connected
@@ -29,7 +65,7 @@ class LinuxCdInfo(CdInfoBase):
     def _get_prefix(self, request):
         return ["cddbcmd", "-m", "http", "cddb", request]
 
-    def _get_track_names(self, disc_id):
+    def _get_track_names_from_cddb(self, disc_id):
         output = list()
         processed_disc_id = disc_id.strip().split(" ")
 
